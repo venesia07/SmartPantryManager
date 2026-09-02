a0f8722bc957995e906519f76587b5be5020b040
VenesiaSwartz <vswartz0605@gmail.com>
2026-09-02 21:54:03 +0200

    Added 'add ingredient' form and pantry navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,8 @@ plugins {
 
 android {
     namespace = "com.example.smartpantrymanager"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
+
 
     defaultConfig {
         applicationId = "com.example.smartpantrymanager"
